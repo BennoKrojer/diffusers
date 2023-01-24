fc326e3f8c1a2b889871bb112f84f387ba47926a
BennoKrojer <benno.krojer@gmail.com>
2023-01-24 17:31:24 -0500

    first finished eval script

--- evaluate.py
+++ evaluate.py
@@ -13,6 +13,7 @@ import clip
 
 from dataset.datasets import get_dataset
 from torch.utils.data import DataLoader
+from utils import evaluate_scores
 
 from src.diffusers import StableDiffusionText2Latentmodelline, StableDiffusionImg2Latentmodelline
 
@@ -21,51 +22,53 @@ class Scorer:
         self.metric = args.metric
         if self.metric == 'clip':
             self.clip_model, self.preprocess = clip.load('ViT-L/14@336px', device='cuda:0')
+        else:
+            vae = AutoencoderKL.from_pretrained('/stable-diffusion-v1-5', subfolder='vae', use_auth_token='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')
+            vae.to(device='cuda', dtype=torch.bfloat16)
+            self.vae_model = StableDiffusionImg2LatentPipeline(vae).to('cuda')
 
+    def score_batch(self, args, batch, model, vae_model):
+        """
+        Takes a batch of images and captions and returns a score for each image-caption pair.
+        """
 
-    def score_batch(self, args, batch, model):
         imgs, texts = batch[0], batch[1]
 
         scores = []
 
         for text in texts:
             if args.txt2img:
-                img, latent = model(prompt=text)
-                
-            else:
-                result_imgs = []
-                for img in imgs:
-                    img, latent = model(prompt=text, init_image=img)
-                    result_imgs.append((img, latent))
-                result[text] = result_imgs
-        
-        return result
-        
-        
-    # img00, latent00 = model(prompt=cap0, init_image=img0, strength=args.strength, num_inference_steps=args.num_inference_steps, guidance_scale=args.guidance_scale)
-    # img01, latent01 = model(prompt=cap0, init_image=img1, strength=args.strength, num_inference_steps=args.num_inference_steps, guidance_scale=args.guidance_scale)
-    # img10, latent10 = model(prompt=cap1, init_image=img0, strength=args.strength, num_inference_steps=args.num_inference_steps, guidance_scale=args.guidance_scale)
-    # img11, latent11 = model(prompt=cap1, init_image=img1, strength=args.strength, num_inference_steps=args.num_inference_steps, guidance_scale=args.guidance_scale)
+                gen, latent = model(prompt=text)
+            for img in imgs:
+                if not args.txt2img:
+                    gen, latent = model(prompt=text, init_image=img)
+                score = self.score_pair(img, gen, latent)
+                scores.append(score)
 
-    # image0 = preprocess(image0).unsqueeze(0).cuda()
-    # image1 = preprocess(image1).unsqueeze(0).cuda()
-    # with torch.no_grad():
-    #     caption_latent0 = clip_model.encode_image(image0).squeeze().float()
-    #     caption_latent1 = clip_model.encode_image(image1).squeeze().float()
+        return scores
+        
+    def score_pair(self, img, gen, latent):
+        """
+        Takes a batch of images and a batch of generated images and returns a score for each image pair.
+        """
+        if self.metric == 'clip':
+            img = self.preprocess(img).unsqueeze(0).cuda()
+            gen = self.preprocess(gen).unsqueeze(0).cuda()
+            with torch.no_grad():
+                img_latent = self.clip_model.encode_image(img).squeeze().float()
+                gen_latent = self.clip_model.encode_image(gen).squeeze().float()
+        else:
+            gen_latent = latent
+            img_latent = self.vae_model(img)
 
-    # original_image0 = Image.open(os.path.join('winoground/images', f'ex_{i}_img_0.png'))
-    # original_image1 = Image.open(os.path.join('winoground/images', f'ex_{i}_img_1.png')) 
-    # original_image0 = preprocess(original_image0).unsqueeze(0).cuda()
-    # original_image1 = preprocess(original_image1).unsqueeze(0).cuda()
-    # with torch.no_grad():
-    #     img_latent0 = clip_model.encode_image(original_image0).squeeze().float()
-    #     img_latent1 = clip_model.encode_image(original_image1).squeeze().float()
+        score = torch.dot(img_latent - gen_latent, img_latent - gen_latent)
+        score = - score # lower is better since it is a similarity metric
+        return score
+
 
 
 def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    clip_model, preprocess = clip.load('ViT-L/14@336px', device='cuda:0')
 
     model_id_or_path = "./stable-diffusion-v1-5"
     if args.txt2img:
@@ -78,6 +81,7 @@ def main(args):
             model_id_or_path,
             safety_checker=None
         )
+
     model = model.to(device)
 
     dataset = get_dataset(args.task, f'datasets/{args.task}')
@@ -85,17 +89,17 @@ def main(args):
     scorer = Scorer(args)
 
     for i, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
-        scorer.process_batch(args, batch, clip_model, preprocess)
+        scores = scorer.process_batch(args, batch, model, vae_model)
+        evaluate_scores(args, scores, batch)
 
 if __name__ == '__main__':
 
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str)
     parser.add_argument('--img2img', action='store_true')
     parser.add_argument('--similarity', type=str, default='clip')
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--cache_dir',type=str)
+    parser.add_argument('--cache_out_dir',type=str, default='')
     args = parser.parse_args()
 
     random.seed(args.seed)
